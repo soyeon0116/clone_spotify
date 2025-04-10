baa0d354f0d983a8034f5672541b68b8c105a03e
김소연 <ksy9252@naver.com>
2025-04-10 16:41:15 +0900

    Feat : 질문 영역 transition 추가 / footer 영역 작업 완료

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 let showFunction = (e) => {
   e.target.classList.toggle('show');
+  e.target.nextElementSibling.classList.toggle('show');
   const status = e.target.classList.contains('show');
   const arrowTag = e.target.children[0];
-  const targetTag = e.target.nextElementSibling;
 
-  if(status) {
-    arrowTag.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
-    targetTag.classList.remove('hidden');
-  } else {
-    arrowTag.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
-    targetTag.classList.add('hidden');
-  }
+  arrowTag.innerHTML = status ? '<i class="fa-solid fa-chevron-up"></i>' : '<i class="fa-solid fa-chevron-down"></i>';
 }
 
 
