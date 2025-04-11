7ea9beeea3b8aed371916aa7ee6c7acf72e669ce
김소연 <ksy9252@naver.com>
2025-04-11 16:41:56 +0900

    Feat :  모바일 퍼스트로 반응형 추가 작업중

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let showFunction = (e) => {
+let showQNAFn = (e) => {
   e.target.classList.toggle('show');
   e.target.nextElementSibling.classList.toggle('show');
   const status = e.target.classList.contains('show');
@@ -7,8 +7,12 @@ let showFunction = (e) => {
   arrowTag.innerHTML = status ? '<i class="fa-solid fa-chevron-up"></i>' : '<i class="fa-solid fa-chevron-down"></i>';
 }
 
-
+let mobileUlFn = (e) => {
+  e.target.classList.toggle('show');
+}
 
 document.querySelectorAll('.questionDiv').forEach(div => {
-  div.addEventListener('click', showFunction);
+  div.addEventListener('click', showQNAFn);
 })
+
+document.querySelector('.hamburger').addEventListener('click', mobileUlFn)
